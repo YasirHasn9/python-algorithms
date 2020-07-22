332c01e0c537e79c1f8a5b1678b382829f6d1973
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-22 15:11:01 -0700

    find single number function is passed successfully

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,16 @@
 '''
-Input: a List of integers where every int except one shows up twice
-Returns: an integer
+
 '''
+
+
 def single_number(arr):
     # Your code here
-
-    pass
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        for num in arr:
+            if arr.count(num) == 1:
+                return num
 
 
 if __name__ == '__main__':
