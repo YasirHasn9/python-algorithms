d0a4114ac2459c1c787aa6de8fd20fdbbd09d192
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-22 23:18:44 -0700

    product all the other numbers except the one at the current index

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,9 +4,33 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    # allocate memory for both left and right
+    left = [0] * len(arr)
+    right = [0] * len(arr)
 
-    pass
+    # the first element in the left is always 1
+    left[0] = 1
+    # the last element in the right element is always 1
+    right[len(arr) - 1] = 1
 
+    prod = [0] * len(arr)
+
+
+
+    #[1,2,3,4]
+    for i in range(1, len(arr)):  
+        # left[0] = 1  
+        left[i] = arr[i - 1] * left[i - 1]
+    
+
+    for i in range(len(arr) - 2 , -1 , -1):
+        # right[i] = 1
+        right[i] = arr[i + 1] * right[i + 1]
+
+    for i in range(len(arr)):
+        prod[i] = left[i] * right[i]
+
+    return prod
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
